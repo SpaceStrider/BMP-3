278ca73c75953e96e60ea7510b0d90104189a947
tonimontana0328 <luis_weber@gmx.de>
2024-07-25 18:26:47 +0200

    Spilt 30mm into 2

---
+++ scripts/Game/Weapon/TEMP_BMP3MagazineWell.c
@@ -0,0 +1,14 @@
+//2A72 30mm HE
+class MagazineWell30mm2A72HEClass {}
+MagazineWell30mm2A72HEClass MagazineWell30mm2A72HESource;
+
+class MagazineWell30mm2A72HE : BaseMagazineWell
+{
+};
+//2A72 30mm AP
+class MagazineWell30mm2A72APClass {}
+MagazineWell30mm2A72APClass MagazineWell30mm2A72APSource;
+
+class MagazineWell30mm2A72AP : BaseMagazineWell
+{
+};
